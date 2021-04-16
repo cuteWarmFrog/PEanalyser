f65125d5a947d6c60a1af79c116667742107ce3a
cuteWarmFrog <livshits.victor@gmail.com>
2021-04-16 20:32:19 +0300

    temp commit for Arsentiy check some shit

--- main.cpp
+++ main.cpp
@@ -36,7 +36,12 @@ bool checker() {
 }
 
 int main() {
+    FILE *PE_FILE = fopen("SpotifySetup.exe", "rb");
+    FILE *BIN_FILE = fopen("bin.txt", "wb");
+
     peFile.open("SpotifySetup.exe", ios::in | ios::binary);
+    ofstream output ("output.txt");
+
     if(checker()) {
         return 0;
     }
@@ -61,26 +66,48 @@ int main() {
         cout << "Cannot reach section headers" << endl;
         return 0;
     }
-    cout << hex << showbase << left;
 
-    cout << "Address of entry point: " << nt_header.OptionalHeader.AddressOfEntryPoint << endl;
+    //Entry point
+    output << "Address of entry point: " << nt_header.OptionalHeader.AddressOfEntryPoint << endl;
+
+    output << hex << showbase << left;
 
     WORD numberOfSections = nt_header.FileHeader.NumberOfSections;
+
+    DWORD pCode;
+    DWORD sizeOfCode;
+
     for(WORD i = 0; i < numberOfSections; i++) {
         IMAGE_SECTION_HEADER sectionHeader;
         peFile.read(reinterpret_cast<char*>(&sectionHeader), sizeof(IMAGE_SECTION_HEADER));
-        cout << "Name: " << sectionHeader.Name << endl;
-        cout << "Virtual address: " << hex << sectionHeader.VirtualAddress << endl;
-        cout << "Characteristics: " << hex << sectionHeader.Characteristics << endl;
-        cout << "Misc physical address: " << hex << sectionHeader.Misc.PhysicalAddress << endl;
-        cout << "Misc virtual size: " << hex << sectionHeader.Misc.VirtualSize << endl;
-        cout << "Number of Relocations: " << hex << sectionHeader.NumberOfRelocations << endl;
-        cout << "Pointer to line numbers: " << hex << sectionHeader.PointerToLinenumbers << endl;
-        cout << "Pointer to raw data: " << hex << sectionHeader.PointerToRawData << endl;
-        cout << "Pointer to relocations: " << hex << sectionHeader.PointerToRelocations << endl;
-        cout << "Size of raw data: " << hex << sectionHeader.SizeOfRawData << endl;
-        cout << endl;
+        output << "Name: " << sectionHeader.Name << endl;
+        output << "Virtual address: " << hex << sectionHeader.VirtualAddress << endl;
+        output << "Characteristics: " << hex << sectionHeader.Characteristics << endl;
+        output << "Misc physical address: " << hex << sectionHeader.Misc.PhysicalAddress << endl;
+        output  << "Misc virtual size: " << hex << sectionHeader.Misc.VirtualSize << endl;
+        output << "Number of Relocations: " << hex << sectionHeader.NumberOfRelocations << endl;
+        output << "Pointer to line numbers: " << hex << sectionHeader.PointerToLinenumbers << endl;
+        output << "Pointer to raw data: " << hex << sectionHeader.PointerToRawData << endl;
+        output << "Pointer to relocations: " << hex << sectionHeader.PointerToRelocations << endl;
+        output << "Size of raw data: " << hex << sectionHeader.SizeOfRawData << endl;
+        output << endl;
+
+        if(sectionHeader.Characteristics & 0x20) {
+            pCode = sectionHeader.PointerToRawData;
+            sizeOfCode = sectionHeader.SizeOfRawData;
+        }
     }
+
+    fseek(PE_FILE, pCode, SEEK_SET);
+    char *code = new char[sizeOfCode];
+
+    fread(&code, sizeof(byte), sizeOfCode, PE_FILE);
+    fwrite(&code, sizeof(byte), sizeOfCode, BIN_FILE);
+
+
+
+
+
 
     /*todo получить исполняемую часть файла в байтах
      * она лежит в .text. читаем с .text size байт
